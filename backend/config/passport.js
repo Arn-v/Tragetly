904e0e5ba0cdc2facf21acd33000e5641836ae6c
Arn-v <singharinav69@gmail.com>
2025-06-02 15:44:26 +0530

    changes in passport.js

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/api/auth/google/callback'
+  callbackURL: process.env.GOOGLE_CALLBACK_URL || '/api/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     let user = await User.findOne({ googleId: profile.id });
